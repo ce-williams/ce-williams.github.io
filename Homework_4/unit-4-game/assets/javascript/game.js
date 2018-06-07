042217d80a9b475cfe6a4ea1cf2ea1fed4ad059e
cewilliams <cewilliams@csbsju.edu>
2018-06-06 23:25:37 -0500

    HW4jsupdates

---
+++ Homework_4/unit-4-game/assets/javascript/game.js
@@ -0,0 +1,31 @@
+var crystals = {
+    blueCrystal: {
+        value: "0",
+        color: "blue"
+    },
+    greenCrystal: {
+        value: "0",
+        color: "green"
+    },
+    whiteCrystal: {
+        value: "0",
+        color: "white"
+    },
+    yellowCrystal: {
+        value: "0",
+        color: "yellow"
+    }
+};
+
+var values = {
+    goalValueVar: "0", 
+    user: {
+        userTotalVar: "0",
+        userWins: "0",
+        userLosses: "0"
+    }
+};
+
+
+
+$(".blue")
