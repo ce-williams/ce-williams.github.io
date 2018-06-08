03e7032497422a41a8a0bab777a2ad09fa890d30
cewilliams <cewilliams@csbsju.edu>
2018-06-07 23:56:41 -0500

    updates

--- Homework_4/unit-4-game/assets/javascript/game.js
+++ Homework_4/unit-4-game/assets/javascript/game.js
@@ -1,30 +1,96 @@
-var crystals = {
-    blueCrystal: {
-        value: "0",
+var crystals = [
+    {
+        value: 0,
         color: "blue"
     },
-    greenCrystal: {
-        value: "0",
+    {
+        value: 0,
         color: "green"
     },
-    whiteCrystal: {
-        value: "0",
+    {
+        value: 0,
         color: "white"
     },
-    yellowCrystal: {
-        value: "0",
+    {
+        value: 0,
         color: "yellow"
+    }
+];
+
+var values = {
+    goalValueVar: 0,
+    user: {
+        userTotalVar: 0,
+        userWins: 0,
+        userLosses: 0
     }
 };
 
-var values = {
-    goalValueVar: "0", 
-    user: {
-        userTotalVar: "0",
-        userWins: "0",
-        userLosses: "0"
+
+
+
+// if (goalValueVar)
+
+function getRandomInt(min, max) {
+    
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function startNewGame() {
+    values.user.userTotalVar = 0;
+    values.goalValueVar = getRandomInt(5, 15);
+    for (i = 0; i < crystals.length; i++) {
+        crystals[i].value = getRandomInt(3, 5);
     }
-};
+
+
+    $("#totalScore").html(values.user.userTotalVar);
+    $("#numberToBeat").html(values.goalValueVar);
+}
+
+
+
+startNewGame();
+
+function addValueToScore(color) {
+    crystals.forEach(function (crystal) {
+        if (color == crystal.color) {
+            console.log(crystal);
+            values.user.userTotalVar += crystal.value;
+            $("#totalScore").html(values.user.userTotalVar);
+            
+            
+            if (values.user.userTotalVar == values.goalValueVar) {
+                values.user.userWins++;
+                $(".wins:first").html(values.user.userWins)
+                alert("You've won!")
+                startNewGame();
+            } else if (values.user.userTotalVar > values.goalValueVar) {
+                values.user.userLosses++;
+                $(".losses:first").html(values.user.userLosses)
+                alert("You've lost!")
+                startNewGame();
+            }
+
+
+
+        }
+    })
+}
+
+
+
+
+$(".blue:first").on("click", function () {
+    addValueToScore("blue");
+});
+// $(".green:first").addEventListener("click")
+// $(".white:first").addEventListener("click")
+// $(".yellow:first").addEventListener("click")
+
+
+
 
 console.log(crystals);
 console.log(values);
