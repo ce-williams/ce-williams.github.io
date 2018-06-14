8db9e286c0ce0ca7a6bad886fc49f4df3a0df17e
cewilliams <cewilliams@csbsju.edu>
2018-06-13 23:39:04 -0500

    misc_updates

--- Homework_5/TriviaGame/assets/javascript/app.js
+++ Homework_5/TriviaGame/assets/javascript/app.js
@@ -44,6 +44,94 @@ var userChoiceThree = "none"
 var userChoiceFour = "none"
 var userChoiceFive = "none"
 
+// // This code will run as soon as the page loads
+// window.onload = function() {
+    
+//     stopwatch.start;
+//   };
+  
+//   //  Variable that will hold our setInterval that runs the stopwatch
+//   var intervalId;
+  
+//   // prevents the clock from being sped up unnecessarily
+//   var clockRunning = false;
+  
+//   // Our stopwatch object
+//   var stopwatch = {
+  
+//     time: 0,
+//     //lap: 1,
+  
+//     reset: function() {
+  
+//       stopwatch.time = 60000;
+//       //stopwatch.lap = 1;
+  
+//       // DONE: Change the "display" div to "01:00."
+//       $("#display").text("01:00");
+  
+//       // DONE: Empty the "laps" div.
+//       //$("#laps").text("");
+//     },
+//     start: function() {
+  
+//       // DONE: Use setInterval to start the count here and set the clock to running.
+//       if (!clockRunning) {
+//         intervalId = setInterval(stopwatch.count, 1000 --);
+//         clockRunning = true;
+//       }
+//     },
+//     stop: function() {
+  
+//       // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+//       clearInterval(intervalId);
+//       clockRunning = false;
+//     },
+//     // recordLap: function() {
+  
+//     //   // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+//     //   //       and save the result in a variable.
+//     //   var converted = stopwatch.timeConverter(stopwatch.time);
+  
+//     //   // DONE: Add the current lap and time to the "laps" div.
+//     //   $("#laps").append("<p>Lap " + stopwatch.lap + " : " + converted + "</p>");
+  
+//     //   // DONE: Increment lap by 1. Remember, we can't use "this" here.
+//     //   stopwatch.lap++;
+//     // },
+//     count: function() {
+  
+//       // DONE: increment time by 1, remember we cant use "this" here.
+//       stopwatch.time--;
+  
+//       // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+//       //       and save the result in a variable.
+//       var converted = stopwatch.timeConverter(stopwatch.time);
+//       console.log(converted);
+  
+//       // DONE: Use the variable we just created to show the converted time in the "display" div.
+//       $("#display").text(converted);
+//     },
+//     timeConverter: function(t) {
+  
+//       var minutes = Math.floor(t / 60);
+//       var seconds = t - (minutes * 60);
+  
+//       if (seconds < 10) {
+//         seconds = "0" + seconds;
+//       }
+  
+//       if (minutes === 0) {
+//         minutes = "00";
+//       }
+//       else if (minutes < 10) {
+//         minutes = "0" + minutes;
+//       }
+  
+//       return minutes + ":" + seconds;
+//     }
+//   };
+  
 
 //Question One
 
@@ -124,10 +212,14 @@ function submitTest() {
         user.score.incorrect ++
     }
 
+    //stopwatch.stop;
+
     alert ("You had " + user.score.correct + " correct and " + user.score.incorrect + " incorrect.")
 
     user.score.correct = 0
     user.score.incorrect = 0
+
+    //stopwatch.reset;
 
 
 };
