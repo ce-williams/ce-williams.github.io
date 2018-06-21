1660409268d3db433516b61067985d35b80984bd
cewilliams <cewilliams@csbsju.edu>
2018-06-20 22:14:04 -0500

    format changes

--- Homework_6/GiphyApp/assets/JavaScript/app.js
+++ Homework_6/GiphyApp/assets/JavaScript/app.js
@@ -1,49 +1,86 @@
-$(document).ready(function)
+$(document).ready(function () {
 
-var topics = ["Michael", "Jim", "Dwight", "Pam", "Oscar", "Kevin"];
+    var searchesOffice = ["Michael", "Jim", "Dwight", "Pam", "Oscar", "Kevin"];
 
+    function displayGifInfo() {
 
+        // clearDiv();
 
-$("button").on("click", function () {
-    var topic = $(this).attr("data-topic");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var search = $(this).attr("data-name");
+        var searchType = '&random';
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + searchType + "&api_key=DgI71RL9D74xpIehLMBbxFr5zun00MwO&limit=10";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        var results = response.data
-        for (var i = 0; i < results.length; i++) {
-            var topicDiv = $("<div>")
-            var ratingP = $("<p>")
-            ratingP.html(data[i].rating)
-            var topicImage = $("<img>")
-            topicImage.attr("src", data[i].images.fixed_height.url)
-            topicImage.attr("alt", "Image from api")
-            topicDiv.append(ratingP)
-        }
-    });
-});
+        console.log(queryURL);
 
-function renderButtons() {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            
+            var results = response.data;
 
-    $("#buttons-view").empty();
+            for (var i = 0; i < results.length; i++) {
+                
+                var gifDiv = $("<div class='gif'>");
+                var rating = results[i].rating;
+                var pOne = $("<p>").text("Rating: " + rating);
+                // console.log(rating);
 
-    for (var i = 0; i < searches.length; i++) {
+                gifDiv.append(pOne);
 
-        var a = $("<button>");
+                var imgURL = results[i].images.fixed_height_downsampled.url;
+                var image = $("<img>").attr("src", imgURL);
 
-        a.addClass("gif-btn");
+                gifDiv.append(image);
 
-        a.attr("data-name", searches[i]);
-        // Providing the initial button text
-        a.text(searches[i]);
-        // Adding the button to the buttons-view div
-        $("#buttons-view").append(a);
+                $("#gif-results").prepend(gifDiv);
+            }
+        });
     }
-}
+
+    function renderButtons() {
+
+        $("#buttons-view").empty();
+
+        for (var i = 0; i < searchesOffice.length; i++) {
+
+            var a = $("<button>");
+
+            a.addClass("gif-btn");
+
+            a.attr("data-name", searchesOffice[i]);
+            // Providing the initial button text
+            a.text(searchesOffice[i]);
+            // Adding the button to the buttons-view div
+            $("#buttons-view").append(a);
+        }
+    }
+
+    $("#add-gif").on("click", function (event) {
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var search = $("#gif-input").val().trim();
+
+        // Adding movie from the textbox to our array
+        searchesOffice.push(search);
+
+        // Calling renderButtons which handles the processing of our movie array
+        renderButtons();
+
+        // $('#gif-results').html("");
+    });
+
+    // Adding a click event listener to all elements with a class of "movie-btn"
+    $(document).on("click", ".gif-btn", displayGifInfo);
+
+    function clearDiv () {
+        $('#gif-results').html("");
+    };
+
+    // Calling the renderButtons function to display the intial buttons
+    renderButtons();
+
+})
 
 
 // var keyLC = toLowerCase("DgI71RL9D74xpIehLMBbxFr5zun00MwO");
