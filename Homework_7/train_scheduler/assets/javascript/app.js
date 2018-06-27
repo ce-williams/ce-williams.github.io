209d6088f87dc032895ac0f088796663f4446094
cewilliams <cewilliams@csbsju.edu>
2018-06-26 19:21:07 -0500

    js updates

--- Homework_7/train_scheduler/assets/javascript/app.js
+++ Homework_7/train_scheduler/assets/javascript/app.js
@@ -1,12 +1,30 @@
+
 $(document).ready(function(){
-    $(".btn-primary").on("click", function submitForm (){
+
+    
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB3hArzyGE9bs6mD1wcAnDxX8IlWo3ttus",
+    authDomain: "trainschedule-e6d00.firebaseapp.com",
+    databaseURL: "https://trainschedule-e6d00.firebaseio.com",
+    projectId: "trainschedule-e6d00",
+    storageBucket: "",
+    messagingSenderId: "225259029409"
+  };
+  firebase.initializeApp(config);
+
+    $(".btn-primary").click(function submitForm (){
         var trainNameInput = $("#formGroupExampleInput1").val();
         var destinationInput = $("#formGroupExampleInput2").val();
         var firstTrainTimeInput = $("#formGroupExampleInput3").val();
         var frequencyInput = $("#formGroupExampleInput4").val();
-        $("table").prepend(trainNameInput, destinationInput, firstTrainTimeInput, frequencyInput);
-    
+        var markup = "<tr><td>" + trainNameInput + "</td><td>" + destinationInput + "</td><td>" + firstTrainTimeInput + "</td><td>" + frequencyInput + "</td><td>";
+        $("table tbody").append(markup);
     });
+
+
+
 
 
 });
