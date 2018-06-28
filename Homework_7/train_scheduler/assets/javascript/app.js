d6e4c26a0061b95358438f10a84a8352fec38434
cewilliams <cewilliams@csbsju.edu>
2018-06-28 18:42:46 -0500

    prev updates

--- Homework_7/train_scheduler/assets/javascript/app.js
+++ Homework_7/train_scheduler/assets/javascript/app.js
@@ -14,14 +14,49 @@ $(document).ready(function(){
   };
   firebase.initializeApp(config);
 
+  // Create a variable to reference the database
+  var database = firebase.database();
+
+    // Initial values
+    var trainNameInput
+    var destinationInput
+    var firstTrainTimeInput
+    var frequencyInput
+
+
     $(".btn-primary").click(function submitForm (){
+        
+        event.preventDefault();
+
         var trainNameInput = $("#formGroupExampleInput1").val();
         var destinationInput = $("#formGroupExampleInput2").val();
         var firstTrainTimeInput = $("#formGroupExampleInput3").val();
         var frequencyInput = $("#formGroupExampleInput4").val();
         var markup = "<tr><td>" + trainNameInput + "</td><td>" + destinationInput + "</td><td>" + firstTrainTimeInput + "</td><td>" + frequencyInput + "</td><td>";
         $("table tbody").append(markup);
+
+        database.ref().set({
+            trainNameInput: nametrainNameInput,
+            destinationInput: destinationInput,
+            firstTrainTimeInput: firstTrainTimeInput,
+            frequencyInput: frequencyInput
+          });
     });
+
+    // Firebase watcher + initial loader HINT: .on("value")
+    database.ref().on("value", function(snapshot) {
+
+  
+        // Change the HTML to reflect
+        $("#name-display").text(snapshot.val().name);
+        $("#email-display").text(snapshot.val().email);
+        $("#age-display").text(snapshot.val().age);
+        $("#comment-display").text(snapshot.val().comment);
+  
+        // Handle the errors
+      }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+      });
 
 
 
